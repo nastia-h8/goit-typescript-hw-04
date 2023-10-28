ab9ba153a6122c813e73b3ad041283320a2ff3fb
Anastasiia Haliara <an.haliara@gmail.com>
2023-10-28 19:25:02 +0300

    complete tasks

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,9 +1,19 @@
-import React, {useReducer} from "react";
+  import {useReducer} from "react";
+
+type State = {
+  isRequestInProgress: boolean,
+  requestStep: "start" | "pending" | "finished" | "idle",
+}
+
+type Action = {
+  type: "START_REQUEST" | "PENDING_REQUEST" | "FINISH_REQUEST"  | "RESET_REQUEST";
+}
 
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
 };
+   
 
 function requestReducer(state: State, action: Action): State {
   switch (action.type) {
